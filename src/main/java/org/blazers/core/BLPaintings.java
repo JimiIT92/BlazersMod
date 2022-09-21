c8547d551e64dfbd4d70a1da8ae8a5e7585c71e8
JimiIT92 <jimiit92@gmail.com>
2022-09-21 12:07:06 +0200

    Added emote paintings

--- src/main/java/org/blazers/core/BLPaintings.java
+++ src/main/java/org/blazers/core/BLPaintings.java
@@ -7,9 +7,115 @@ import org.blazers.BlazersMod;
 
 public final class BLPaintings {
 
-    public static PaintingVariant ERENBLAZE = register("erenblaze", new PaintingVariant(32, 32));
-    public static PaintingVariant SURVIVAL = register("survival", new PaintingVariant(16, 32));
-    public static PaintingVariant BRUH = register("bruh", new PaintingVariant(16, 16));
+    public static PaintingVariant ERENBLAZE = registerPainting("erenblaze", 32);
+    public static PaintingVariant SURVIVAL = registerPainting("survival", 16, 32);
+    public static PaintingVariant BRUH = registerSmallPainting("bruh");
+    public static PaintingVariant EBL_CHEER = registerSmallPainting("ebl_cheer");
+    public static PaintingVariant EBL_FLEX = registerSmallPainting("ebl_flex");
+    public static PaintingVariant EBL_SHH = registerSmallPainting("ebl_shh");
+    public static PaintingVariant EBL_SHREK = registerSmallPainting("ebl_shrek");
+    public static PaintingVariant EBL_WOW = registerSmallPainting("ebl_wow");
+    public static PaintingVariant EBL_SMILE = registerSmallPainting("ebl_smile");
+    public static PaintingVariant EBL_BAIT = registerSmallPainting("ebl_bait");
+    public static PaintingVariant EBL_POG = registerSmallPainting("ebl_pog");
+    public static PaintingVariant EBL_MON = registerSmallPainting("ebl_mon");
+    public static PaintingVariant EBL_K = registerSmallPainting("ebl_k");
+    public static PaintingVariant EBL_SHY = registerSmallPainting("ebl_shy");
+    public static PaintingVariant EBL_MIA = registerSmallPainting("ebl_mia");
+    public static PaintingVariant EBL_CAPE = registerSmallPainting("ebl_cape");
+    public static PaintingVariant EBL_MID = registerSmallPainting("ebl_mid");
+    public static PaintingVariant EBL_CHUG = registerSmallPainting("ebl_chug");
+    public static PaintingVariant EBL_PERV = registerSmallPainting("ebl_perv");
+    public static PaintingVariant EBL_TROLL = registerSmallPainting("ebl_troll");
+    public static PaintingVariant EBL_10 = registerSmallPainting("ebl_10");
+    public static PaintingVariant EBL_LURK = registerSmallPainting("ebl_lurk");
+    public static PaintingVariant EBL_PALM = registerSmallPainting("ebl_palm");
+    public static PaintingVariant EBL_NO = registerSmallPainting("ebl_no");
+    public static PaintingVariant EBL_GRM = registerSmallPainting("ebl_grm");
+    public static PaintingVariant EBL_HYPE = registerSmallPainting("ebl_hype");
+    public static PaintingVariant EBL_RBT = registerSmallPainting("ebl_rbt");
+    public static PaintingVariant EBL_200 = registerSmallPainting("ebl_200");
+    public static PaintingVariant EBL_BAN = registerSmallPainting("ebl_ban");
+    public static PaintingVariant EBL_HMM = registerSmallPainting("ebl_hmm");
+    public static PaintingVariant EBL_LOVE = registerSmallPainting("ebl_love");
+    public static PaintingVariant EBL_PNO = registerSmallPainting("ebl_pno");
+    public static PaintingVariant EBL_STONKS = registerSmallPainting("ebl_stonks");
+    public static PaintingVariant EBL_WTF = registerSmallPainting("ebl_wtf");
+    public static PaintingVariant EBL_GAO = registerSmallPainting("ebl_gao");
+    public static PaintingVariant EBL_NTL = registerSmallPainting("ebl_ntl");
+    public static PaintingVariant EBL_CLIP = registerSmallPainting("ebl_clip");
+    public static PaintingVariant EBL_COP = registerSmallPainting("ebl_cop");
+    public static PaintingVariant EBL_CRINGE = registerSmallPainting("ebl_cringe");
+    public static PaintingVariant EBL_CHAD = registerSmallPainting("ebl_chad");
+    public static PaintingVariant EBL_F = registerSmallPainting("ebl_f");
+    public static PaintingVariant EBL_TVB = registerSmallPainting("ebl_tvb");
+    public static PaintingVariant EBL_420 = registerSmallPainting("ebl_420");
+    public static PaintingVariant EBL_TRIGGERED = registerSmallPainting("ebl_triggered");
+    public static PaintingVariant EBL_LOL = registerSmallPainting("ebl_lol");
+    public static PaintingVariant EBL_FEAR = registerSmallPainting("ebl_fear");
+    public static PaintingVariant EBL_PTT = registerSmallPainting("ebl_ptt");
+    public static PaintingVariant EBL_RAGE = registerSmallPainting("ebl_rage");
+    public static PaintingVariant EBL_ZZZ = registerSmallPainting("ebl_zzz");
+    public static PaintingVariant EBL_STOMP = registerSmallPainting("ebl_stomp");
+    public static PaintingVariant EBL_UNC = registerSmallPainting("ebl_unc");
+    public static PaintingVariant EBL_GG = registerSmallPainting("ebl_gg");
+    public static PaintingVariant EBL_DERP = registerSmallPainting("ebl_derp");
+    public static PaintingVariant EBL_FOOD = registerSmallPainting("ebl_food");
+    public static PaintingVariant EBL_TH = registerSmallPainting("ebl_th");
+    public static PaintingVariant EBL_SAD = registerSmallPainting("ebl_sad");
+    public static PaintingVariant EBL_GA = registerSmallPainting("ebl_ga");
+    public static PaintingVariant EBL_CLOWN = registerSmallPainting("ebl_clown");
+    public static PaintingVariant EBL_EVIL = registerSmallPainting("ebl_evil");
+    public static PaintingVariant EBL_PRIDE = registerSmallPainting("ebl_pride");
+    public static PaintingVariant EBL_TITAN = registerSmallPainting("ebl_titan");
+    public static PaintingVariant EBL_L = registerSmallPainting("ebl_l");
+    public static PaintingVariant EBL_KILL = registerSmallPainting("ebl_kill");
+    public static PaintingVariant EBL_HI = registerSmallPainting("ebl_hi");
+    public static PaintingVariant EBL_GIFT = registerSmallPainting("ebl_gift");
+    public static PaintingVariant EBL_KEK = registerPainting("ebl_kek", 32);
+    public static PaintingVariant EBL_INSIDE = registerPainting("ebl_inside", 32);
+    public static PaintingVariant EBL_GUN = registerPainting("ebl_gun", 32);
+    public static PaintingVariant EBL_BONK = registerPainting("ebl_bonk", 32);
+    public static PaintingVariant EBL_IH = registerPainting("ebl_ih", 32);
+    public static PaintingVariant EBL_EM = registerPainting("ebl_em", 48);
+    public static PaintingVariant EBL_FURRY = registerPainting("ebl_furry", 48);
+    public static PaintingVariant EBL_UWU = registerPainting("ebl_uwu", 48);
+    public static PaintingVariant EBL_SCREAM = registerPainting("ebl_scream", 48);
+    public static PaintingVariant EBL_DP = registerPainting("ebl_dp", 48);
+
+    /**
+     * Register a {@link PaintingVariant painting} sized 16x16
+     *
+     * @param name {@link String Painting Name}
+     * @return {@link PaintingVariant Registered Painting}
+     */
+    private static PaintingVariant registerSmallPainting(final String name) {
+        return registerPainting(name, 16);
+    }
+
+    /**
+     * Register a {@link PaintingVariant painting} with the same
+     * {@link Integer Width} and {@link Integer Height}
+     *
+     * @param name {@link String Painting Name}
+     * @param size {@link Integer Painting Size}
+     * @return {@link PaintingVariant Registered Painting}
+     */
+    private static PaintingVariant registerPainting(final String name, final int size) {
+        return registerPainting(name, size, size);
+    }
+
+    /**
+     * Register a {@link PaintingVariant painting}
+     *
+     * @param name {@link String Painting Name}
+     * @param width {@link Integer Painting Width}
+     * @param height {@link Integer Painting Height}
+     * @return {@link PaintingVariant Registered Painting}
+     */
+    private static PaintingVariant registerPainting(final String name, final int width, final int height) {
+        return register(name, new PaintingVariant(width, height));
+    }
 
     private static PaintingVariant register(String name, PaintingVariant variant) {
         return Registry.register(Registry.PAINTING_VARIANT, new Identifier(BlazersMod.MOD_ID, name), variant);
