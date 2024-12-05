9a30d8533837c98d59b6d01d81ef0d0ecb959c72
JimiIT92 <jimiit92@gmail.com>
2024-12-05 14:41:16 +0100

    Branch reset for update + integration of HendrixCore

--- src/main/java/org/blazers/BlazersMod.java
+++ src/main/java/org/blazers/BlazersMod.java
@@ -1,41 +1,24 @@
 package org.blazers;
 
 import net.fabricmc.api.ModInitializer;
-import org.blazers.core.*;
-import org.blazers.world.gen.BLEntitiesGeneration;
-import org.blazers.world.gen.BLFlowersGeneration;
-import org.blazers.world.gen.BLOreGeneration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import software.bernie.geckolib.GeckoLib;
+import org.hendrix.HendrixCore;
 
+/**
+ * Blazers Mod. A Minecraft mod made for ErenBlaze
+ */
 public final class BlazersMod implements ModInitializer {
 
+    /**
+     * The {@link String Mod Id}
+     */
     public static final String MOD_ID = "blazersmod";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    /**
+     * Initialize the mod
+     */
     @Override
     public void onInitialize() {
-        BLTabs.registerItemGroup();
-        BLInstruments.register();
-        BLFeatures.register();
-        BLItems.register();
-        BLBlocks.register();
-        BLBlockSetTypes.register();
-        BLPaintings.register();
-        BLScreenHandlers.register();
-        BLRecipes.register();
-        BLLootModifiers.register();
-        BLBlocks.registerWaxedBlocks();
-        BLBlocks.registerOxidizableBlocks();
-        BLBlocks.registerStrippables();
-        BLBlocks.registerFlammableBlocks();
-        BLBlocks.registerDispenserBehaviors();
-        BLEntityTypes.registerAttributes();
-        BLOreGeneration.generateOres();
-        BLFlowersGeneration.generateFlowers();
-        BLEntitiesGeneration.generateEntities();
-
-        GeckoLib.initialize();
+        HendrixCore.init(MOD_ID);
     }
+
 }
--- src/main/java/org/blazers/item/IPreEnchantedItem.java
+++
@@ -1,9 +0,0 @@
-package org.blazers.item;
-
-import com.mojang.datafixers.util.Pair;
-import net.minecraft.enchantment.Enchantment;
-
-public interface IPreEnchantedItem {
-
-    Pair<Enchantment, Integer> getEnchantment();
-}
